996df83d0398b462c4422424acf91b01f1fe4579
Ananth <anan.busyguy007@gmail.com>
2022-08-11 09:44:38 +0200

    Return account when login service success

--- kotlin_app/src/main/java/life/league/challenge/kotlin/repository/LoginRepository.kt
+++ kotlin_app/src/main/java/life/league/challenge/kotlin/repository/LoginRepository.kt
@@ -1,26 +1,33 @@
 package life.league.challenge.kotlin.repository
 
+import android.util.Base64
 import life.league.challenge.kotlin.api.Api
 import life.league.challenge.kotlin.api.ApiResponse
+import life.league.challenge.kotlin.model.Account
+import retrofit2.HttpException
+import java.io.IOException
 
 interface LoginRepository {
-    suspend fun login(username: String, password: String)
+    suspend fun login(username: String, password: String): ApiResponse<Account>
 }
 
-class LoginRepositoryImpl(private val api: Api, private val authorizationHelper: AuthorizationHelper) : LoginRepository {
+class LoginRepositoryImpl(
+    private val api: Api,
+    private val authorizationHelper: AuthorizationHelper
+) : LoginRepository {
 
-    override suspend fun login(username: String, password: String) {
+    override suspend fun login(username: String, password: String): ApiResponse<Account> {
         val authorization = authorizationHelper.create(username, password)
-        api.login(authorization)
+        val result = api.login(authorization)
+        return ApiResponse.Success(data = result)
     }
 }
 
 class AuthorizationHelper {
     fun create(username: String, password: String): String {
-        val authorization = "Basic " + android.util.Base64.encodeToString(
+        return "Basic " + Base64.encodeToString(
             "$username:$password".toByteArray(),
-            android.util.Base64.NO_WRAP
+            Base64.NO_WRAP
         )
-        return authorization
     }
 }
--- kotlin_app/src/test/java/life/league/challenge/kotlin/repository/LoginRepositoryImplTest.kt
+++ kotlin_app/src/test/java/life/league/challenge/kotlin/repository/LoginRepositoryImplTest.kt
@@ -1,10 +1,12 @@
 package life.league.challenge.kotlin.repository
 
 import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.shouldBe
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
 import life.league.challenge.kotlin.api.Api
+import life.league.challenge.kotlin.api.ApiResponse
 import life.league.challenge.kotlin.model.Account
 
 internal class LoginRepositoryImplTest : ShouldSpec({
@@ -22,5 +24,19 @@ internal class LoginRepositoryImplTest : ShouldSpec({
         repository.login(username, password)
 
         coVerify { api.login(authorization) }
+    }
+
+    should("return account when login service success") {
+        val api = mockk<Api>()
+        val authorization = "an authorization"
+        val authorizationHelper = mockk<AuthorizationHelper>()
+        val account = Account("an api key")
+        coEvery { authorizationHelper.create(username, password) } returns authorization
+        coEvery { api.login(authorization) } returns account
+        val repository = LoginRepositoryImpl(api, authorizationHelper)
+
+        val result = repository.login(username, password)
+
+        result shouldBe ApiResponse.Success(account)
     }
 })

0e05677c377703e773685a38e22bf8adb5a2b158
Ananth <anan.busyguy007@gmail.com>
2022-08-11 17:06:31 +0200

    Wire ViewPostsViewModel to fragment

--- kotlin_app/src/main/java/life/league/challenge/kotlin/feature/posts/ViewPostsFragment.kt
+++ kotlin_app/src/main/java/life/league/challenge/kotlin/feature/posts/ViewPostsFragment.kt
@@ -6,11 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.kraftanapp.myapplication.ui.theme.MyApplicationTheme
+import life.league.challenge.kotlin.feature.login.LoginViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ViewPostsFragment : Fragment() {
+
+    private val viewModel: ViewPostsViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,8 +26,11 @@ class ViewPostsFragment : Fragment() {
     ): View {
         return ComposeView(requireContext()).apply {
             setContent {
+
+                val state by remember { viewModel.uiState }.collectAsState()
+
                 MyApplicationTheme {
-                    Greeting("Hello world.")
+                    ViewPostsScreen(state)
                 }
             }
         }

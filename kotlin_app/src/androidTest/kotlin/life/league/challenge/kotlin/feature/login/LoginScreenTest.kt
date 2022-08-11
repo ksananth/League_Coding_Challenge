0ce91995defc2c725efe9d6d0304dd2a990f7f94
Ananth <anan.busyguy007@gmail.com>
2022-08-11 15:23:24 +0200

    Show loading when state is loading

--- kotlin_app/src/androidTest/kotlin/life/league/challenge/kotlin/feature/login/LoginScreenTest.kt
+++ kotlin_app/src/androidTest/kotlin/life/league/challenge/kotlin/feature/login/LoginScreenTest.kt
@@ -1,26 +1,23 @@
 package life.league.challenge.kotlin.feature.login
 
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.ComposeContentTestRule
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.performClick
-import com.kraftanapp.myapplication.ui.theme.MyApplicationTheme
 import org.junit.Rule
 import org.junit.Test
 
 
-internal class LoginFragmentTest {
+ class LoginScreenTest {
 
-    @get:Rule
-    val composeTestRule = createComposeRule()
-
+    @Rule
+    @JvmField
+    var composeTestRule: ComposeContentTestRule = createComposeRule()
 
     @Test
     fun loadingState() {
         composeTestRule.setContent {
-            MyApplicationTheme {
                 LoginScreen(state = LoginViewModel.UIState.Loading)
-            }
         }
 
         composeTestRule.onNodeWithText("Please wait logging in...").assertIsDisplayed()
--- kotlin_app/src/main/java/life/league/challenge/kotlin/feature/login/LoginFragment.kt
+++ kotlin_app/src/main/java/life/league/challenge/kotlin/feature/login/LoginFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember

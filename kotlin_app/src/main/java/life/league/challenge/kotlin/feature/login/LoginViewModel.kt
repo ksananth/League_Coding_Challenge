e25c21742ea47aeee11416cd8be468bed90eef19
Ananth <anan.busyguy007@gmail.com>
2022-08-11 15:44:14 +0200

    Refactor: extract to contant

--- kotlin_app/src/main/java/life/league/challenge/kotlin/feature/login/LoginViewModel.kt
+++ kotlin_app/src/main/java/life/league/challenge/kotlin/feature/login/LoginViewModel.kt
@@ -8,12 +8,15 @@ import kotlinx.coroutines.launch
 import life.league.challenge.kotlin.api.ApiResponse
 import life.league.challenge.kotlin.repository.LoginRepository
 
+private const val USERNAME = "hello"
+private const val PASSWORD = "world"
+
 class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel() {
 
     private val _uiState: MutableStateFlow<UIState> by lazy {
         MutableStateFlow<UIState>(UIState.Loading).apply {
             viewModelScope.launch {
-                when (val result = loginRepository.login("hello", "world")) {
+                when (val result = loginRepository.login(USERNAME, PASSWORD)) {
                     is ApiResponse.ApiError -> emit(UIState.Error)
                     is ApiResponse.NoInternetError -> emit(UIState.NoInternet)
                     is ApiResponse.Success -> emit(UIState.NavigateToPosts(result.data.apiKey))

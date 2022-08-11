913816efedb4f25e197c26fe98def7c3e0dadd12
Ananth <anan.busyguy007@gmail.com>
2022-08-11 12:15:56 +0200

    Refactor: Remove unused variables

--- kotlin_app/src/main/java/life/league/challenge/kotlin/api/Service.kt
+++ kotlin_app/src/main/java/life/league/challenge/kotlin/api/Service.kt
@@ -6,7 +6,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 object Service {
 
     private const val HOST = "https://engineering.league.dev/challenge/api/"
-    private const val TAG = "Service"
 
     val api: Api by lazy {
         val retrofit = Retrofit.Builder()
--- kotlin_app/src/main/java/life/league/challenge/kotlin/main/MainActivity.kt
+++ kotlin_app/src/main/java/life/league/challenge/kotlin/main/MainActivity.kt
@@ -1,28 +1,14 @@
 package life.league.challenge.kotlin.main
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import life.league.challenge.kotlin.R
-import life.league.challenge.kotlin.api.ApiResponse
-import life.league.challenge.kotlin.api.Service
-import life.league.challenge.kotlin.api.login
-import life.league.challenge.kotlin.repository.AuthorizationHelper
-import life.league.challenge.kotlin.repository.LoginRepository
-import life.league.challenge.kotlin.repository.LoginRepositoryImpl
 
 class MainActivity : AppCompatActivity() {
-
-    companion object {
-        private const val TAG = "MainActivity"
-    }
 
     private lateinit var toolbar: Toolbar
     private lateinit var appBarConfiguration: AppBarConfiguration

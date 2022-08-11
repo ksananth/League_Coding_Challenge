2b1dc9af63d9b7cd62eb74b29e35a7c4b1103f20
Ananth <anan.busyguy007@gmail.com>
2022-08-11 15:42:25 +0200

    Show posts when state is data

--- kotlin_app/src/androidTest/kotlin/life/league/challenge/kotlin/feature/posts/ViewPostsScreenKtTest.kt
+++ kotlin_app/src/androidTest/kotlin/life/league/challenge/kotlin/feature/posts/ViewPostsScreenKtTest.kt
@@ -3,9 +3,9 @@ package life.league.challenge.kotlin.feature.posts
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.ComposeContentTestRule
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
-import life.league.challenge.kotlin.feature.login.LoginScreen
-import life.league.challenge.kotlin.feature.login.LoginViewModel
+import life.league.challenge.kotlin.model.Post
 import org.junit.Rule
 import org.junit.Test
 
@@ -41,6 +41,21 @@ internal class ViewPostsScreenKtTest {
         }
 
         composeTestRule.onNodeWithText("Please check your internet connections.").assertIsDisplayed()
+    }
+
+    @Test
+    fun showPost() {
+        val title = "a title"
+        val body = "a body"
+
+        composeTestRule.setContent {
+            val aPost = Post("1", "id", title, body)
+            ViewPostsScreen(state = ViewPostsViewModel.UIState.Data(listOf(aPost)))
+        }
+
+        composeTestRule.onNodeWithText(title).assertIsDisplayed()
+        composeTestRule.onNodeWithText(body).assertIsDisplayed()
+        composeTestRule.onNodeWithTag(TEST_TAG_IMAGE).assertIsDisplayed()
     }
 
 }
--- kotlin_app/src/main/java/life/league/challenge/kotlin/feature/posts/ViewPostsScreen.kt
+++ kotlin_app/src/main/java/life/league/challenge/kotlin/feature/posts/ViewPostsScreen.kt
@@ -1,17 +1,61 @@
 package life.league.challenge.kotlin.feature.posts
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import life.league.challenge.kotlin.R
 import life.league.challenge.kotlin.composables.ErrorScreen
 import life.league.challenge.kotlin.composables.Loading
 import life.league.challenge.kotlin.composables.NoInternetScreen
-import life.league.challenge.kotlin.feature.login.LoginViewModel
+import life.league.challenge.kotlin.model.Post
+
+const val TEST_TAG_IMAGE = "IMAGE"
 
 @Composable
 internal fun ViewPostsScreen(state: ViewPostsViewModel.UIState) {
-    when(state){
-        is ViewPostsViewModel.UIState.Data -> TODO()
+    when (state) {
+        is ViewPostsViewModel.UIState.Data -> ShowPosts(state.data)
         ViewPostsViewModel.UIState.Error -> ErrorScreen()
         ViewPostsViewModel.UIState.Loading -> Loading("Fetching posts...")
         ViewPostsViewModel.UIState.NoInternet -> NoInternetScreen()
+    }
+}
+
+@Composable
+fun ShowPosts(data: List<Post>) {
+    LazyColumn {
+        items(data.size) { index ->
+            PostCard(data[index])
+        }
+    }
+}
+
+@Composable
+fun PostCard(msg: Post) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .testTag("IMAGE")
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = msg.title)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
     }
 }

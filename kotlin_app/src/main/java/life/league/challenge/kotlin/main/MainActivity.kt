39011892b7b134031e5af63d283b3b963f349baf
Ananth <anan.busyguy007@gmail.com>
2022-08-11 08:08:43 +0200

    Setup toolbar with navController

--- kotlin_app/src/main/java/life/league/challenge/kotlin/main/MainActivity.kt
+++ kotlin_app/src/main/java/life/league/challenge/kotlin/main/MainActivity.kt
@@ -3,7 +3,11 @@ package life.league.challenge.kotlin.main
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import life.league.challenge.kotlin.R
@@ -16,9 +20,20 @@ class MainActivity : AppCompatActivity() {
         private const val TAG = "MainActivity"
     }
 
+    private lateinit var toolbar: Toolbar
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private val navController by lazy {
+        (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.mainToolBar)
+        setSupportActionBar(toolbar)
+        appBarConfiguration = AppBarConfiguration(navController.graph, null)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     override fun onResume() {

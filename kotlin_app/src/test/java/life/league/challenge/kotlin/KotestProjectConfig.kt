24d252762a613ff7bacedebd71fab6caf118ad89
Ananth <anan.busyguy007@gmail.com>
2022-08-11 11:52:21 +0200

    Add KotestProjectConfig to inject CoroutinesTestListener

---
+++ kotlin_app/src/test/java/life/league/challenge/kotlin/KotestProjectConfig.kt
@@ -0,0 +1,57 @@
+package life.league.challenge.kotlin
+
+import androidx.compose.runtime.ExperimentalComposeApi
+import io.kotest.core.config.AbstractProjectConfig
+import io.kotest.core.extensions.Extension
+import io.kotest.core.listeners.TestListener
+import io.kotest.core.spec.IsolationMode
+import io.kotest.core.spec.Spec
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import kotlin.time.Duration
+
+class KotestProjectConfig : AbstractProjectConfig() {
+    override val timeout: Duration?
+        get() = super.timeout
+    override val isolationMode: IsolationMode
+        get() = IsolationMode.InstancePerLeaf
+
+    override fun extensions(): List<Extension> =
+        listOf(CoroutinesTestListener)
+}
+
+@OptIn(ExperimentalComposeApi::class)
+object CoroutinesTestListener : TestListener {
+    val dispatcher: CoroutineDispatcher get() = testDispatcher
+    private val testDispatcher = TestCoroutineDispatcher()
+
+    override suspend fun beforeSpec(spec: Spec) {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override suspend fun afterSpec(spec: Spec) {
+        Dispatchers.resetMain()
+    }
+}
+
+class TestDispatchersProvider(private val dispatcher: CoroutineDispatcher) : DispatcherProvider {
+    override val io: CoroutineDispatcher
+        get() = dispatcher
+    override val unconfined: CoroutineDispatcher
+        get() = dispatcher
+    override val main: CoroutineDispatcher
+        get() = dispatcher
+    override val default: CoroutineDispatcher
+        get() = dispatcher
+}
+
+interface DispatcherProvider {
+    val io: CoroutineDispatcher get() = Dispatchers.IO
+    val unconfined: CoroutineDispatcher get() = Dispatchers.Unconfined
+    val main: CoroutineDispatcher get() = Dispatchers.Main
+    val default: CoroutineDispatcher get() = Dispatchers.Default
+}
+

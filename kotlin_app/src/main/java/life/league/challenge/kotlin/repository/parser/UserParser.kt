dcfdaf7d93bfda0feb4e6de0817c614e6aa2f46a
Ananth <anan.busyguy007@gmail.com>
2022-08-13 00:15:32 +0200

    Refactor: Extract method hasObjectOrNull

--- kotlin_app/src/main/java/life/league/challenge/kotlin/repository/parser/UserParser.kt
+++ kotlin_app/src/main/java/life/league/challenge/kotlin/repository/parser/UserParser.kt
@@ -1,6 +1,7 @@
 package life.league.challenge.kotlin.repository.parser
 
 import com.google.gson.JsonElement
+import com.google.gson.JsonObject
 import life.league.challenge.kotlin.domain.User
 
 class UserParser {
@@ -8,12 +9,15 @@ class UserParser {
         return result.asJsonArray.map {
             val userObject = it.asJsonObject
             val id = userObject["id"].asInt
-            val avatar = if (userObject.has("avatar")) {
-                userObject["avatar"].asString
-            } else null
-            val username = userObject["username"].asString
+            val avatar = userObject.hasObjectOrNull("avatar")
+            val username = userObject.hasObjectOrNull("username")
 
             User(id, avatar, username)
         }
     }
+
+    private fun JsonObject.hasObjectOrNull(key: String) =
+        if (this.has(key)) {
+            this["key"].asString
+        } else null
 }

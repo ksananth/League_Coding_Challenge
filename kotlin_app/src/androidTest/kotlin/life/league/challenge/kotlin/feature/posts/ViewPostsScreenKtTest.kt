622ef97fdf4429bf58e3cefc7898ea03e8ee26eb
Ananth <anan.busyguy007@gmail.com>
2022-08-13 01:32:12 +0200

    Fix ui tests

--- kotlin_app/src/androidTest/kotlin/life/league/challenge/kotlin/feature/posts/ViewPostsScreenKtTest.kt
+++ kotlin_app/src/androidTest/kotlin/life/league/challenge/kotlin/feature/posts/ViewPostsScreenKtTest.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.test.junit4.ComposeContentTestRule
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import life.league.challenge.kotlin.domain.UserPost
 import life.league.challenge.kotlin.model.Post
 import org.junit.Rule
 import org.junit.Test
@@ -47,13 +48,15 @@ internal class ViewPostsScreenKtTest {
     fun showPost() {
         val title = "a title"
         val body = "a body"
+        val username = "an user name"
 
         composeTestRule.setContent {
-            val aPost = Post("1", "id", title, body)
+            val aPost = UserPost("an avatar", "an user name", title, body)
             ViewPostsScreen(state = ViewPostsViewModel.UIState.Data(listOf(aPost)))
         }
 
         composeTestRule.onNodeWithText(title).assertIsDisplayed()
+        composeTestRule.onNodeWithText(username).assertIsDisplayed()
         composeTestRule.onNodeWithText(body).assertIsDisplayed()
         composeTestRule.onNodeWithTag(TEST_TAG_IMAGE).assertIsDisplayed()
     }

7490bbac077aa3653d3d6094090a7172988b6710
Ananth <anan.busyguy007@gmail.com>
2022-08-13 00:54:48 +0200

    Update toolbar style

--- kotlin_app/src/main/java/life/league/challenge/kotlin/main/MainActivity.kt
+++ kotlin_app/src/main/java/life/league/challenge/kotlin/main/MainActivity.kt
@@ -1,11 +1,14 @@
 package life.league.challenge.kotlin.main
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import life.league.challenge.kotlin.R
 
 class MainActivity : AppCompatActivity() {
@@ -19,9 +22,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         toolbar = findViewById(R.id.mainToolBar)
+
+        val mTitle: TextView = toolbar.findViewById<View>(R.id.toolbar_title) as TextView
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navHostFragment.navController.addOnDestinationChangedListener { _, destination, arguments ->
+            mTitle.text = destination.label
+        }
         setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
         appBarConfiguration = AppBarConfiguration(setOf(
             R.id.loginFragment,
             R.id.viewPostsFragment,
